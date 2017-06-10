2b323df49f93e66f59b9514d7f1b9462a047dc0a
kisv701 <kisv701@gmail.com>
2017-06-11 01:14:28 +0200

    Missed a few files

--- js/Track.js
+++ js/Track.js
@@ -3,32 +3,54 @@
  */
 function Track() {
 
-
     this.wsin = random(40,80);   //Random weight for one of the sines
     this.wsin2 = random(20,40);  //Random weight for one of the sines
     var n = 1000;               //Number of points
     var aStep = 2*PI/n;         //Angle step
 
+    //Starting y position
+    this.sy = function(){
+        return height*0.5 | 0;
+    };
+    //Starting x position
+    this.sx = function(){
+        console.log("sx: ",200+this.wsin2*sin(-PI/3) | 0);
+        return (width*0.5 + 200+this.wsin2*sin(-PI/3) | 0);
+    };
+    //Starting angle
+    this.sa = function () {
+        var l = 10*aStep; //length of hop for derivative
+        var r = 200+this.wsin*sin(2*l)+this.wsin2*sin(4*l-PI/3);
+        var nexty = height*0.5 + Math.floor(r*Math.sin(l));
+        var nextx = width*0.5 + Math.floor(r*Math.cos(l));
+        return Math.atan2(this.sy()-nexty, this.sx() - nextx);
+
+    };
+
     this.isOnTrack = function(x,y){
         return this.pix[(x+y*width)];
     };
 
-    this.draw = function () {
+    this.show = function () {
         //Generate a track
-
+        
         var a = 0;                  //Starting Angle
 
         while(a < 2*PI){
             var r = 200+this.wsin*sin(2*a)+this.wsin2*sin(4*a-PI/3);
-            var x = Math.floor(r*Math.sin(a));
-            var y = Math.floor(r*Math.cos(a));
+            var y = height*0.5 + Math.floor(r*Math.sin(a));
+            var x = width*0.5 + Math.floor(r*Math.cos(a));
             a+= aStep;
             noStroke();
             fill(225);
             ellipse(x,y,70,70);
         }
 
+    };
 
+    this.init = function () {
+        this.show();
+        var x,y;
         loadPixels();
         this.pix =Array((pixels.length/4 | 0));
         for(y=0;y< height; y++){
@@ -46,6 +68,4 @@ function Track() {
         }
         updatePixels();
     };
-
-    this.draw();
 }
--- js/main.js
+++ js/main.js
@@ -4,17 +4,31 @@
 var track;
 var lastTimeStamp = Date.now();
 var deltaT = 10; // 10 ms for each timestep.
+var car;
+var dwAngl = 0; //Drive wheel angle
+var gasPedal = 0; //Gas pedal level (can go to -1 for reverse)
+var forwadPressed = false;  //Keep track of which keys are pressed, drive with WASD
+var backwardPressed = false;
+var leftPressed = false;
+var rightPressed = false;
 
 //P5.js Main functions
 function setup(){
     createCanvas(700,700);
     pixelDensity(1);
     background(51);
-    translate(width/2,height/2,0,0);
     track = new Track();
+    track.init();
+    var carx = track.sx();
+    var cary = track.sy();
+    car = new Car(carx,cary,track.sa());
 }
 function draw(){
 
+    handleInputs();
+    car.applyForce(dwAngl,-gasPedal);
+    background(51);
+    track.show();
     //Initialize a timedebt that the simulation much run through to catch up to the actual time.
     //I want constant timesteps so therefore a dt is used. Makes for better simulation
     var currentTime = Date.now();
@@ -26,19 +40,80 @@ function draw(){
         counter++;
     }
     lastTimeStamp = currentTime;
-
+    car.show();
     //console.log(counter); //Making sure update runs accordingly
 
 }
 function mouseClicked() {
     if(track.isOnTrack(mouseX,mouseY)){
-        alert("on track");
+        //alert("on track");
+        console.log("Hit","X:" + mouseX +  ", Y:" + mouseY);
+    } else {
+        console.log("Miss","X:" + mouseX +  ", Y:" + mouseY);
+    }
+}
+
+function keyPressed(){
+    switch (keyCode) {
+        case 87:    //w
+            forwadPressed =true;
+            break;
+        case 83:    //s
+            backwardPressed =true;
+            break;
+        case 65:    //a
+            leftPressed = true;
+            break;
+        case 68:    //d
+            rightPressed = true;
+            break;
+    }
+}
+
+function keyReleased() {
+    switch (keyCode) {
+        case 87:    //w
+            forwadPressed =false;
+            gasPedal=0;
+            break;
+        case 83:    //s
+            backwardPressed =false;
+            gasPedal=0;
+            break;
+        case 65:    //a
+            leftPressed = false;
+            dwAngl=0;
+            break;
+        case 68:    //d
+            rightPressed = false;
+            dwAngl=0;
+            break;
     }
 }
 
 //Project specific functions
 
+function handleInputs() {
+    if(forwadPressed){
+        gasPedal += 0.2;
+        gasPedal = (gasPedal > 1) ? 1:gasPedal;
+    }
+    if(backwardPressed){
+        gasPedal -= 0.2;
+        gasPedal = (gasPedal < -1) ? -1:gasPedal;
+    }
+    if(leftPressed){
+        dwAngl -= 0.2;
+        dwAngl = (dwAngl < -1) ? -1:dwAngl;
+    }
+    if(rightPressed){
+        dwAngl += 0.2;
+        dwAngl = (dwAngl > 1) ? 1:dwAngl;
+    }
+}
 //Simulation timestep forward
 function update(dt){
+    //Project specific functions
+    car.update(dt);
 
 }
